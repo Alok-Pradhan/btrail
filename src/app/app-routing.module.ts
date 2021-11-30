85aa45d9aafb56196cf2dce5622f3fba0e9c72bb
Alok-Pradhan <alokpradhan196@gmail.com>
2021-11-30 14:51:47 +0530

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page01Component } from './page01/page01.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'page01', component:Page01Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page01Component } from './page01/page01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page01Component
   ],
   imports: [
     BrowserModule,
